dd9d56edcba37f5771dc88db6a9ec85305cb3c34
Manuel Navarro Perez <manuelnavarro1987@gmail.com>
2018-01-04 17:28:53 +0100

    Change style in Login component

--- src/components/Auth/Login/index.jsx
+++ src/components/Auth/Login/index.jsx
@@ -1,15 +1,82 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { Button } from 'material-ui'
+import { withStyles } from 'material-ui/styles'
+import {
+  Button,
+  Grid,
+  Paper,
+  TextField
+} from 'material-ui'
 
-export default function Login() {
+const styles = (theme) => ({
+  paper: {
+    padding: 16,
+    textAlign: 'center'
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  rowButtons: {
+    marginTop: 30
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+})
+
+function Login(props) {
+  const { classes } = props
+
   return (
     <div>
-      <Button component={Link} to="register" raised color="primary">
-        Register
-      </Button>
+      <Grid container justify="center" alignItems="center">
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
 
-      <p>Login</p>
+            <form className={classes.form} noValidate>
+              <TextField
+                id="username"
+                label="Username"
+                margin="normal"
+                fullWidth
+                required
+              />
+              <TextField
+                id="password"
+                type="password"
+                label="Password"
+                margin="normal"
+                fullWidth
+                required
+              />
+              <Grid className={classes.rowButtons} container justify="space-between" alignItems="center">
+                <Grid item>
+                  <Button component={Link} to="register" color="primary">
+                    Register
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button component={Link} to="register" raised color="primary" disabled>
+                    Submit
+                    <span className={classes.rightIcon}>
+                      <i class="material-icons">send</i>
+                    </span>
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   )
 }
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Login)
