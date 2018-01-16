d49841a0c8f887e17b7e97d61093a9bb6c5fee50
Manuel Navarro Perez <manuelnavarro1987@gmail.com>
2018-01-16 21:23:59 +0100

    New business container component

---
+++ src/containers/Business.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Redirect, Switch, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import BusinessList from '../components/Business/BusinessList'
+import BusinessForm from '../components/Business/BusinessForm'
+
+function Business({ match, business = [], onNewBusinessClick }) {
+  return (
+    <div>
+      <Switch>
+        <Route path={`${match.url}/list`} render={withRouter(({ history }) => <BusinessList business={business} history={history} />)} />
+        <Route path={`${match.url}/new`} render={withRouter(({ history }) => <BusinessForm onSubmit={onNewBusinessClick} history={history} />)} />
+        <Redirect from={`${match.url}/`} to={`${match.url}/list`} />
+      </Switch>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    business: state.business
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    onNewBusinessClick: () => console.log('new Business')
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Business)
