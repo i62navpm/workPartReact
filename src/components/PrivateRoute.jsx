493bbdab93ea5de4ffa855be5c3d130a24d629df
Manuel Navarro Perez <manuelnavarro1987@gmail.com>
2018-01-17 17:02:43 +0100

    New private route component

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route {...rest} render={props => (
+      true ? (
+        <Component {...props} />
+      ) : (
+          <Redirect to={{
+            pathname: '/auth/login',
+            state: { from: props.location }
+          }} />
+        )
+    )} />
+  )
+}
