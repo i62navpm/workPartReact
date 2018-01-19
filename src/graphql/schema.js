604b563481b169589ee6b8dc734c1c125adac14b
Manuel Navarro Perez <manuelnavarro1987@gmail.com>
2018-01-19 21:22:35 +0100

    Change mock data

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -28,11 +28,11 @@ const businessData = [
     id: '1',
     name: 'Business example 1',
     date: DateTime.local().toLocaleString(DateTime.DATETIME_MED),
-    cif: 'X14111222',
-    address: 'Company address',
-    phone: '000000000',
-    email: 'company@email.com',
-    web: 'http://www.companyweb.com',
+    cif: 'X14111211',
+    address: 'Company address1',
+    phone: '110000000',
+    email: 'company1@email.com',
+    web: 'http://www.companyweb1.com',
     image: imageBusiness
   },
   {
@@ -40,21 +40,21 @@ const businessData = [
     name: 'New business example 2',
     date: DateTime.local().toLocaleString(DateTime.DATETIME_MED),
     cif: 'X14111222',
-    address: 'Company address',
-    phone: '000000000',
-    email: 'company@email.com',
-    web: 'http://www.companyweb.com',
+    address: 'Company address2',
+    phone: '220000000',
+    email: 'company2@email.com',
+    web: 'http://www.companyweb2.com',
     image: imageBusiness
   },
   {
     id: '3',
     name: 'Example business 3',
     date: DateTime.local().toLocaleString(DateTime.DATETIME_MED),
-    cif: 'X14111222',
-    address: 'Company address',
-    phone: '000000000',
-    email: 'company@email.com',
-    web: 'http://www.companyweb.com',
+    cif: 'X14111223',
+    address: 'Company address3',
+    phone: '330000000',
+    email: 'company3@email.com',
+    web: 'http://www.companyweb3.com',
     image: imageBusiness
   }
 ]
