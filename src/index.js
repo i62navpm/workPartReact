abf1d7d30f30488598b41785b6d5efa01c0598a2
Manuel Navarro Perez <manuelnavarro1987@gmail.com>
2018-01-19 11:51:39 +0100

    Apollo client graphql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { applyMiddleware, createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
@@ -8,13 +8,24 @@ import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import storeApp from './reducers'
+import { ApolloProvider } from 'react-apollo'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://1jzxrj179.lp.gql.zone/graphql' }),
+  cache: new InMemoryCache()
+})
 
 const store = createStore(storeApp, compose(applyMiddleware(thunk, logger)))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 )
 registerServiceWorker()
