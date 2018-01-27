210d9acc994b85f7dd59cf61d9127863970b8623
Manuel Navarro Perez <manuelnavarro1987@gmail.com>
2018-01-27 13:15:41 +0100

    New workforce mock

---
+++ src/graphql/workforceMock.js
@@ -0,0 +1,40 @@
+import { DateTime } from 'luxon'
+import imageBusiness from '../assets/images/businessDefault.png'
+
+export default [
+  {
+    id: '1',
+    name: 'Employee example 1',
+    nif: '14111211X',
+    address: 'Employee address1',
+    phone: '110000000',
+    email: 'employee1@email.com',
+    image: imageBusiness,
+    fullSalary: 45.5,
+    halfSalary: 25
+  },
+  {
+    id: '2',
+    name: 'New employee example 2',
+    date: DateTime.local().toLocaleString(DateTime.DATETIME_MED),
+    nif: '14111222X',
+    address: 'Employee address2',
+    phone: '220000000',
+    email: 'employee2@email.com',
+    image: imageBusiness,
+    fullSalary: 45.5,
+    halfSalary: 25
+  },
+  {
+    id: '3',
+    name: 'Example employee 3',
+    date: DateTime.local().toLocaleString(DateTime.DATETIME_MED),
+    nif: '14111223X',
+    address: 'Employee address3',
+    phone: '330000000',
+    email: 'employee3@email.com',
+    image: imageBusiness,
+    fullSalary: 45.5,
+    halfSalary: 25
+  }
+]
