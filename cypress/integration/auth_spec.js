e20b413737803d74dd3ca2c1002d824ae5608fc7
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-02-19 16:57:52 +0100

    Stub response in auth components

--- cypress/integration/auth_spec.js
+++ cypress/integration/auth_spec.js
@@ -1,4 +1,4 @@
-import { env, user, mockUser } from '../../src/config/config'
+import { env, user } from '../../src/config/config'
 
 describe('Auth test', function () {
   beforeEach(function () {
@@ -7,14 +7,14 @@ describe('Auth test', function () {
 
   it('Submit the login component', function () {
     const url = '/auth/login'
-    
+
     cy.visit(env + url)
     cy.contains('Login')
     cy.url().should('include', url)
-    
+
     cy.get('#username').type(user.username)
     cy.get('#password').type(user.password)
-    
+
     cy.get('form').submit()
 
     cy.url().should('include', '/business')
@@ -27,32 +27,29 @@ describe('Auth test', function () {
     cy.contains('Register')
     cy.url().should('include', url)
 
-    cy.get('#email').type(mockUser.username)
-    cy.get('#password').type(mockUser.password)
+    cy.get('#email').type(user.username)
+    cy.get('#password').type(user.password)
 
     cy.server()
-    cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/').as('userRegister')
+
+    cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/', {}).as('userRegister')
     cy.get('form').submit()
-
     cy.wait('@userRegister').then((xhr) => {
-      if (xhr.status === 400) {
-        expect(xhr.status).to.equal(400)
-        cy.url().should('include', url)
-      } else if (xhr.status === 200) {
-        expect(xhr.status).to.equal(200)
-
-        cy.contains('Verification')
-
-        cy.get('#verificationCode').type('123456')
-
-        cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/').as('userVerificateEmail')
-        cy.get('form').submit()
-        cy.wait('@userVerificateEmail').then((xhr) => {
-          expect(xhr.status).to.equal(400)
-          cy.url().should('include', 'auth/verification')
-        })
-      }
+      expect(xhr.status).to.equal(200)
     })
+
+    cy.url().should('include', '/auth/verification')
+    cy.contains('Verification')
+
+    cy.get('#verificationCode').type('123456')
+
+    cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/', {}).as('userVerificateCode')
+    cy.get('form').submit()
+    cy.wait('@userVerificateCode').then((xhr) => {
+      expect(xhr.status).to.equal(200)
+    })
+
+    cy.url().should('include', '/auth/login')
   })
 
   it('Submit the forgot password component', function () {
@@ -66,27 +63,24 @@ describe('Auth test', function () {
 
     cy.server()
 
-    cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/').as('userSendEmail')
+    cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/', {}).as('userSendEmail')
     cy.get('button[type="button"]').click()
-    
     cy.wait('@userSendEmail').then((xhr) => {
-      expect(xhr.status).to.equal(400)
-      
-      if (xhr.status === 200) {
-        cy.contains('Verification')
-
-        cy.get('#password').type(user.password)
-        cy.get('#verificationCode').type('123456')
-
-        cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/').as('userForgotPassword')
-        cy.get('form').submit()
-        cy.wait('@userForgotPassword').then((xhr) => {
-          expect(xhr.status).to.equal(400)
-        })
-      }
+      expect(xhr.status).to.equal(200)
     })
 
-    cy.url().should('include', url)
+    cy.contains('Verification')
+
+    cy.get('#verificationCode').type('123456')
+    cy.get('#password').type(user.password)
+
+    cy.route('POST', 'https://cognito-idp.eu-central-1.amazonaws.com/', {}).as('userForgotPassword')
+    cy.get('form').submit()
+    cy.wait('@userForgotPassword').then((xhr) => {
+      expect(xhr.status).to.equal(200)
+    })
+
+    cy.url().should('include', '/auth/login')
   })
 
 })
