b0676e71e282829872f973693798375b412e245e
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-02-26 22:37:23 +0100

    Format employee summary

--- src/components/Employee/EmployeeSummary.jsx
+++ src/components/Employee/EmployeeSummary.jsx
@@ -14,14 +14,21 @@ import {
   Divider,
   colors
 } from 'material-ui'
-import { TagFaces, Close, Home, Phone, Mail, FormatQuote } from 'material-ui-icons'
+import {
+  TagFaces,
+  Close,
+  Home,
+  Phone,
+  Mail,
+  FormatQuote
+} from 'material-ui-icons'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 import SummaryBarChart from './SummaryBarChart'
 
 const randomColor = function (obj) {
   const keys = Object.keys(obj)
-  return obj[keys[keys.length * Math.random() << 0]]
+  return obj[keys[(keys.length * Math.random()) << 0]]
 }
 
 const styles = theme => ({
@@ -54,16 +61,16 @@ const styles = theme => ({
   },
   description: {
     fontStyle: 'italic',
-    marginBottom: theme. spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2
   },
   monthLineChart: {
     height: '250px',
     flex: 1
-  },
+  }
 })
 
 class EmployeeSummary extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.props.setLoader(true)
 
@@ -78,14 +85,14 @@ class EmployeeSummary extends React.Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps (nextProps) {
     const { data: { employeeSummary, loading } } = nextProps
 
     this.setState({ loading, employeeSummary })
     this.props.setLoader(loading)
   }
 
-  render() {
+  render () {
     const { loading } = this.state
 
     if (loading) return null
@@ -118,69 +125,94 @@ class EmployeeSummary extends React.Component {
                   justify={'flex-start'}
                 >
                   <Grid item>
-                    <Avatar aria-label="employee-summary" style={{ backgroundColor: randomColor(colors)[500] }}>
+                    <Avatar
+                      aria-label="employee-summary"
+                      style={{ backgroundColor: randomColor(colors)[500] }}
+                    >
                       {this.state.employeeSummary.name[0]}
                     </Avatar>
                   </Grid>
                   <Grid item>
-                    <Typography color="primary" type="headline">{this.state.employeeSummary.name}</Typography>
-                    <Typography type="subheading">{this.state.employeeSummary.nif}</Typography>
+                    <Typography color="primary" type="headline">
+                      {this.state.employeeSummary.name}
+                    </Typography>
+                    <Typography type="subheading">
+                      {this.state.employeeSummary.nif}
+                    </Typography>
                   </Grid>
                 </Grid>
               </Grid>
 
-              {this.state.employeeSummary.description && <Grid item xs={12}>
+              {this.state.employeeSummary.description && (
+                <Grid item xs={12}>
+                  <Grid
+                    container
+                    alignItems={'center'}
+                    direction={'row'}
+                    justify={'center'}
+                  >
+                    <Grid item xs={10} sm={8}>
+                      <Typography
+                        type="caption"
+                        className={this.classes.description}
+                        align="center"
+                      >
+                        <FormatQuote />
+                        {this.state.employeeSummary.description}
+                      </Typography>
+                      <Divider light />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              )}
+
+              <Grid item xs={12} sm={4} md={3}>
+                <Grid container>
+                  <Grid item>
+                    <Home className={this.classes.icons} />
+                  </Grid>
+                  <Grid item>
+                    <Typography type="body1">
+                      {this.state.employeeSummary.address}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item>
+                    <Phone className={this.classes.icons} />
+                  </Grid>
+                  <Grid item>
+                    <Typography type="body1">
+                      {this.state.employeeSummary.phone}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item>
+                    <Mail className={this.classes.icons} />
+                  </Grid>
+                  <Grid item>
+                    <Typography type="body1">
+                      <a href={`mailto:${this.state.employeeSummary.nif}`}>
+                        {this.state.employeeSummary.email}
+                      </a>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              <Grid item xs={12} sm={8} md={9}>
                 <Grid
                   container
                   alignItems={'center'}
                   direction={'row'}
                   justify={'center'}
                 >
-                  <Grid item xs={10} sm={8} >
-                    <Typography type="caption" className={this.classes.description} align="center"><FormatQuote />{this.state.employeeSummary.description}</Typography>
-                    <Divider light/>
-                  </Grid>
-                </Grid>
-              </Grid>}
-
-              <Grid item xs={12} sm={4} md={3}>
-                <Grid container>
-                  <Grid item>
-                    <Home className={this.classes.icons}/>
-                  </Grid>
-                  <Grid item>
-                    <Typography type="body1">{this.state.employeeSummary.address}</Typography>
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid item>
-                    <Phone className={this.classes.icons}/>
-                  </Grid>
-                  <Grid item>
-                    <Typography type="body1">{this.state.employeeSummary.phone}</Typography>
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid item>
-                    <Mail className={this.classes.icons}/>
-                  </Grid>
-                  <Grid item>
-                    <Typography type="body1">
-                      <a href={`mailto:${this.state.employeeSummary.nif}`}>{this.state.employeeSummary.email}</a>
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-
-
-              <Grid item xs={12} sm={8} md={9}>
-                <Grid container alignItems={'center'} direction={'row'} justify={'center'}>
                   <Grid item className={this.classes.monthLineChart}>
                     <SummaryBarChart data={this.state.employeeSummary.events} />
                   </Grid>
                 </Grid>
               </Grid>
-
             </Grid>
           </Paper>
         </Grid>
@@ -200,39 +232,48 @@ EmployeeSummary.propTypes = {
   companyId: PropTypes.string.isRequired
 }
 
-export default graphql(gql`
-  fragment EventSummary on Event {
-    data {
-      money
-    },
-    start
-  }
-  query getEmployeeSummary($companyId: ID, $employeeId: ID, $date: String) {
-    employeeSummary(companyId: $companyId, employeeId: $employeeId, date: $date) {
-      id,
-      name,
-      description,
-      nif,
-      address,
-      email,
-      phone,
-      events {
-        pay {
-          ...EventSummary
-        },
-        debt {
-          ...EventSummary
+export default graphql(
+  gql`
+    fragment EventSummary on Event {
+      data {
+        money
+      }
+      start
+    }
+
+    query getEmployeeSummary($companyId: ID, $employeeId: ID, $date: String) {
+      employeeSummary(
+        companyId: $companyId
+        employeeId: $employeeId
+        date: $date
+      ) {
+        id
+        name
+        description
+        nif
+        address
+        email
+        phone
+        events {
+          pay {
+            ...EventSummary
+          }
+          debt {
+            ...EventSummary
+          }
+        }
+      }
+    }
+  `,
+  {
+    options: ({ match, companyId }) => {
+      return {
+        variables: {
+          companyId,
+          employeeId: match.params.employeeId,
+          date: new Date().toISOString()
         }
       }
     }
   }
-  `, {
-    options: ({ match, companyId }) => {
-      return { variables: { companyId, employeeId: match.params.employeeId, date: new Date().toISOString() } }
-    }
-  })(
-    connect(
-      null,
-      mapDispatchToProps
-    )(withStyles(styles)(EmployeeSummary))
-  )
+)(connect(null, mapDispatchToProps)(withStyles(styles)(EmployeeSummary)))
