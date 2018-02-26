4c59608382cc216f9d14bdda793e2d4dba6e137a
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-02-26 20:42:48 +0100

    New summary component

---
+++ src/components/Employee/EmployeeSummary.jsx
@@ -0,0 +1,209 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { setLoader } from '../../actions/loader'
+import { withStyles } from 'material-ui/styles'
+import {
+  Grid,
+  Paper,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton
+} from 'material-ui'
+import { Avatar } from 'material-ui'
+import { TagFaces, Close, Home, Phone, Mail, FormatQuote } from 'material-ui-icons'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+const styles = theme => ({
+  paper: {
+    padding: 16,
+    textAlign: 'center'
+  },
+  root: {
+    flexGrow: 1,
+    textAlign: 'left'
+  },
+  rowSubmit: {
+    marginTop: 30
+  },
+  iconLeft: {
+    marginRight: theme.spacing.unit,
+    width: 32,
+    height: 32
+  },
+  iconRight: {
+    marginLeft: theme.spacing.unit,
+    width: 20,
+    height: 20
+  },
+  flex: {
+    flex: 1
+  },
+  description: {
+    fontStyle: 'italic'
+  }
+})
+
+class EmployeeSummary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.props.setLoader(true)
+
+    const { classes } = props
+    this.classes = classes
+
+    const { loading, employeeSummary, ...data} = props.data
+    this.state = {
+      loading,
+      employeeSummary,
+      data
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { data: { employeeSummary, loading } } = nextProps
+
+    this.setState({ loading, employeeSummary })
+    this.props.setLoader(loading)
+  }
+
+  render() {
+    const { loading } = this.state
+
+    if (loading) return null
+
+    return (
+      <Grid container justify="center" alignItems="center">
+        <Grid item xs={12} sm={10}>
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <TagFaces className={this.classes.iconLeft} />
+              <Typography
+                type="title"
+                color="inherit"
+                className={this.classes.flex}
+              >
+                Employee Summary
+              </Typography>
+              <IconButton onClick={this.props.closeForm} color="contrast">
+                <Close />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Paper className={this.classes.paper}>
+            <Grid container className={this.classes.root}>
+              <Grid item xs={12}>
+                <Grid
+                  container
+                  alignItems={'center'}
+                  direction={'row'}
+                  justify={'flex-start'}
+                >
+                  <Grid item>
+                    <Avatar aria-label="Employee">A</Avatar>
+                  </Grid>
+                  <Grid item>
+                    <Typography color="primary" type="headline">{this.state.employeeSummary.name}</Typography>
+                    <Typography type="subheading">{this.state.employeeSummary.nif}</Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              {this.state.employeeSummary.description && <Grid item xs={12}>
+                <Grid
+                  container
+                  alignItems={'center'}
+                  direction={'row'}
+                  justify={'center'}
+                >
+                  <Grid xs={10} sm={8} item>
+                    <Typography type="caption" className={this.classes.description} align="center"><FormatQuote />{this.state.employeeSummary.description}</Typography>
+                  </Grid>
+                </Grid>
+              </Grid>}
+
+              <Grid item xs={12} sm={8}>
+                <Grid container>
+                  <Grid item>
+                    <Home />
+                  </Grid>
+                  <Grid item>
+                    <Typography type="body1">{this.state.employeeSummary.address}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item>
+                    <Phone />
+                  </Grid>
+                  <Grid item>
+                    <Typography type="body1">{this.state.employeeSummary.phone}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item>
+                    <Mail />
+                  </Grid>
+                  <Grid item>
+                    <Typography type="body1">
+                      <a href={`mailto:${this.state.employeeSummary.nif}`}>{this.state.employeeSummary.email}</a>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setLoader: loading => dispatch(setLoader({ loading }))
+  }
+}
+
+EmployeeSummary.propTypes = {
+  classes: PropTypes.object.isRequired,
+  companyId: PropTypes.string.isRequired
+}
+
+export default graphql(gql`
+  fragment EventSummary on Event {
+    data {
+      money
+    },
+    start
+  }
+  query getEmployeeSummary($companyId: ID, $employeeId: ID, $date: String) {
+    employeeSummary(companyId: $companyId, employeeId: $employeeId, date: $date) {
+      id,
+      name,
+      description,
+      nif,
+      address,
+      email,
+      phone,
+      events {
+        pay {
+          ...EventSummary
+        },
+        debt {
+          ...EventSummary
+        }
+      }
+    }
+  }
+  `, {
+    options: ({ match, companyId }) => {
+      return { variables: { companyId, employeeId: match.params.employeeId, date: new Date().toISOString() } }
+    }
+  })(
+    connect(
+      null,
+      mapDispatchToProps
+    )(withStyles(styles)(EmployeeSummary))
+  )
--- src/components/Employee/index.js
+++ src/components/Employee/index.js
@@ -2,5 +2,12 @@ import EmployeeCard from './EmployeeCard'
 import EmployeeCalendar from './EmployeeCalendar'
 import EmployeeForm from './EmployeeForm'
 import EmployeeList from './EmployeeList'
+import EmployeeSummary from './EmployeeSummary'
 
-export { EmployeeCard, EmployeeCalendar, EmployeeForm, EmployeeList }
+export {
+  EmployeeCard,
+  EmployeeCalendar,
+  EmployeeForm,
+  EmployeeList,
+  EmployeeSummary
+}
