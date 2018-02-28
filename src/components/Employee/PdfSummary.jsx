df36d2e9e91f5c6629ce912e506db540c7df2b8b
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-02-28 15:10:09 +0100

    Hidden pdf in xs

--- src/components/Employee/PdfSummary.jsx
+++ src/components/Employee/PdfSummary.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Typography } from 'material-ui'
+import { Typography, Hidden } from 'material-ui'
 import { withStyles } from 'material-ui/styles'
 import jsPDF from 'jspdf'
 
 const styles = theme => ({
   titleSummary: {
     marginTop: theme.spacing.unit * 4,
-    marginBottom: theme.spacing.unit * 4 
+    marginBottom: theme.spacing.unit * 4
   }
 })
 
@@ -21,13 +21,12 @@ function PdfSummary(props) {
 
   return (
     <React.Fragment>
-      <Typography className={props.classes.titleSummary} align="center" type="headline" color="primary">
-        Employee Summary
+      <Hidden xsDown>
+        <Typography className={props.classes.titleSummary} align="center" type="headline" color="primary">
+          Employee Summary
       </Typography>
-      <iframe src={doc.output('datauristring')} type="application/pdf" width="100%" height="842px">
-        
-      </iframe>
-    
+        <iframe title="Summary employee pdf" src={doc.output('datauristring')} type="application/pdf" width="100%" height="842px" />
+      </Hidden>
     </React.Fragment>
   )
 }
