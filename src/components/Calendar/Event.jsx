972e63f8e1aef5c766856fd533f95cd7d48fe729
Manuel Navarro Pérez <manuelnavarro1987@gmail.com>
2018-04-06 20:41:47 +0200

    New work icon in event component

--- src/components/Calendar/Event.jsx
+++ src/components/Calendar/Event.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
 
 import { IconButton } from 'material-ui'
-import { Face } from 'material-ui-icons'
+import { LocalShipping, Face } from 'material-ui-icons'
 
 export default function Event({ event, classes, onSetEvent }) {
   return (
     <React.Fragment>
       <span className={'event-title'}>{event.title}</span>
-      <span className={'event-money'}>
-        {!event.money ? (
-          <IconButton
-            variant="raised"
-            color="primary"
-            className={classes.smallIconButton}
-            onClick={e => onSetEvent(e, event)}
-          >
-            <Face className={classes.smallIcon} />
-          </IconButton>
-        ) : (
-            `${event.money} €`
-          )}
+      <span className={'event-work'}>
+        <IconButton
+          variant="raised"
+          color="primary"
+          className={classes.smallIconButton}
+          onClick={() => { }}
+        >
+          <LocalShipping className={classes.smallIcon} />
+        </IconButton>
       </span>
+      {!event.money ? (
+        <IconButton
+          variant="raised"
+          color="primary"
+          className={`event-money ${classes.smallIconButton}`}
+          onClick={e => onSetEvent(e, event)}
+        >
+          <Face className={classes.smallIcon} />
+        </IconButton>
+      ) : (
+          <span className={'money-detail'}>  
+            ${event.money} €
+          </span>
+        )}
     </React.Fragment>
   )
 }
